4dc00e9614ad8fd4a3c13a757ac41fd38fe5f7d9
Leozin89Python <leozin069@gmail.com>
2020-12-25 22:09:44 -0300

    exercicios-java-v1.1.3

--- spring-backend/demo/src/main/java/com/server/config/testConfig.java
+++ spring-backend/demo/src/main/java/com/server/config/testConfig.java
@@ -8,10 +8,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.server.entities.Category;
 import com.server.entities.Order;
+import com.server.entities.Product;
 import com.server.entities.User;
 import com.server.entities.Enums.OrderStatus;
+import com.server.repositories.CategoryRepository;
 import com.server.repositories.OrderRepository;
+import com.server.repositories.ProductRepository;
 import com.server.repositories.UserRepository;
 
 
@@ -24,8 +28,24 @@ public class testConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository  OrderRepository;
+	
+	@Autowired
+	private CategoryRepository CategoryRepository; 
+	
+	@Autowired
+	private  ProductRepository ProductRepository;
 
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "123456","988888888");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com",  "123456","977777777"); 
@@ -34,8 +54,13 @@ public class testConfig implements CommandLineRunner{
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.paid,u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.waitting_payment, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.delivered, u1);
-
+		
+		
+		
 		UserRepository.saveAll(Arrays.asList(u1,u2));
 		OrderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		CategoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		ProductRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
 	}		
 }
